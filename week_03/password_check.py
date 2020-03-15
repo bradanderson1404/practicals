ada4d9d84bcfe75e2d906c20ceed8fbd2e63b0fa
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2020-03-15 22:17:27 +1000

    refactor password check program to use functions

--- week_03/password_check.py
+++ week_03/password_check.py
@@ -1,7 +1,21 @@
-password = input("Enter Password: ")
-while len(password) < 5:
-    print("Password cannot be shorter than 5 characters.")
-    password = input("Re-enter password: ")
-print("Password Saved:", end=' ')
-for i in range(len(password)):
-    print("*", end='')
+def main():
+
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("Password Saved:", end=' ')
+    for i in range(len(password)):
+        print("*", end='')
+
+
+def get_password():
+    password = input("Enter Password: ")
+    while len(password) < 5:
+        print("Password cannot be shorter than 5 characters.")
+        password = input("Re-enter password: ")
+    return password
+
+
+main()
