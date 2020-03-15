0fc96475b26c3f7b68395afbdb7cec1c0746ea38
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2020-03-15 22:09:05 +1000

    create practical repository

---
+++ week_02/exceptions_demo.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
+
+# 1. When a non-integer is entered for numerator or denominator
+# 2. If a 0 is entered into the denominator
+# 3. Yes
+
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Cannot divide by zero!")
+        denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+print("Finished.")
---
+++ week_02/randoms.py
@@ -0,0 +1,14 @@
+# On line 1 I saw a 12
+# The smallest I could've seen was a 5 and the highest a 20
+
+# On line 2 I saw a 5
+# The smallest I could've seen was a 3 and the highest a 9
+# A 4 could not be produced by the line 2 code
+
+# On line 3 I saw 4.474481...
+# The smallest I could've seen was a 2.5 and the highest a 5.5
+
+import random
+
+number = random.randint(1, 100)
+print(number)
