26917bf0c5b188da491b72f530b9b2ef4e59e00c
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2020-03-15 22:33:45 +1000

    refactor score program to use a function

--- week_03/broken_score.py
+++ week_03/broken_score.py
@@ -1,9 +1,17 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    print(determine_result(score))
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
