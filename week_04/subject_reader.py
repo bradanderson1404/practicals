eaed414e38567ac62634e85c3874c0bcfcb17cd9
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2020-03-21 22:04:01 +1000

    extract display method

--- week_04/subject_reader.py
+++ week_04/subject_reader.py
@@ -7,23 +7,28 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    class_lists = get_data()
+    display_subject_details(class_lists)
+
+
+def display_subject_details(class_lists):
+    """Format and display the subject details."""
+    for i in range(0, len(class_lists)):
+        print("{} is taught by {:12} and has {:>3} students".format(class_lists[i][0], class_lists[i][1],
+                                                                    class_lists[i][2]))
 
 
 def get_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+    """Read data from file and return class information in a list of lists"""
+    class_lists = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        class_lists.append(parts)
     input_file.close()
+    return class_lists
 
 
 main()
