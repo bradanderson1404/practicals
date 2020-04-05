ab0678173835531c02a4fd5518d287ff450801dd
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2020-04-05 23:09:46 +1000

    add prompt to check if name is correct

--- week_05/emails.py
+++ week_05/emails.py
@@ -5,5 +5,14 @@ email = input("Enter your email: ")
 name_portion, domain = email.split("@")
 if "." in name_portion:
     firstname, surname = name_portion.split(".")
+    prompt = input("Is your name {} {}? Y/N".format(firstname, surname))
+    full_name = " ".join(firstname, surname)
+    while prompt.upper() == "N":
+        full_name = input("Enter your name: ")
+    user_details[email] = full_name
 else:
     firstname = name_portion
+    prompt = input("Is your name {}? Y/N".format(firstname))
+    while prompt.upper() == "N":
+        full_name = input("Enter your name: ")
+    user_details[email] = firstname
