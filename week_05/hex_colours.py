97b8c449bce7f3e4495dbaa30db0d07a98cffe10
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2020-04-05 21:50:41 +1000

    add string formatting to print dictionary

--- week_05/hex_colours.py
+++ week_05/hex_colours.py
@@ -1,3 +1,6 @@
 HEX_COLOURS = {"Cyan1": "#00ffff", "BlueViolet": "#8a2be2", "DarkSeaGreen": "#8fbc8f", "FireBrick2": "#ee2c2c",
                "GhostWhite": "#f8f8ff", "GreenYellow": "#adff2f", "HotPink": "#ff69b4",
                "Lavender": "#e6e6fa", "NavyBlue": "#000080", "Wheat1": "#ffe7ba"}
+
+for key in HEX_COLOURS:
+    print("{:>12s} is {}".format(key, HEX_COLOURS[key]))
