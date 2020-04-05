257e38facd882bd47618612d49874cf567e74236
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2020-04-05 23:13:12 +1000

    simplify code

--- week_05/emails.py
+++ week_05/emails.py
@@ -1,18 +1,19 @@
 user_details = {}
 
 email = input("Enter your email: ")
+while email != "" and "@" in email:
+    name_portion, domain = email.split("@")
+    if "." in name_portion:
+        name = name_portion.split(".")
+        full_name = " ".join(name)
+        prompt = input("Is your name {}? Y/N".format(name))
+        while prompt.upper() == "N":
+            full_name = input("Enter your name: ")
+        user_details[email] = full_name
+    else:
+        name = name_portion
+        prompt = input("Is your name {}? Y/N".format(name))
+        while prompt.upper() == "N":
+            full_name = input("Enter your name: ")
+        user_details[email] = name
 
-name_portion, domain = email.split("@")
-if "." in name_portion:
-    firstname, surname = name_portion.split(".")
-    prompt = input("Is your name {} {}? Y/N".format(firstname, surname))
-    full_name = " ".join(firstname, surname)
-    while prompt.upper() == "N":
-        full_name = input("Enter your name: ")
-    user_details[email] = full_name
-else:
-    firstname = name_portion
-    prompt = input("Is your name {}? Y/N".format(firstname))
-    while prompt.upper() == "N":
-        full_name = input("Enter your name: ")
-    user_details[email] = firstname
