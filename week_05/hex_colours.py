f73e313d424c2c59ede6ce83ffee2ba3c6961fe2
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2020-04-05 21:52:55 +1000

    add error checking

--- week_05/hex_colours.py
+++ week_05/hex_colours.py
@@ -4,3 +4,12 @@ HEX_COLOURS = {"Cyan1": "#00ffff", "BlueViolet": "#8a2be2", "DarkSeaGreen": "#8f
 
 for key in HEX_COLOURS:
     print("{:>12s} is {}".format(key, HEX_COLOURS[key]))
+
+colour = input("Enter name of colour: ")
+while colour != "":
+    if colour in HEX_COLOURS:
+        print(colour + " is " + HEX_COLOURS[colour])
+    else:
+        print("Invalid colour name.")
+    colour = input("Enter name of colour: ")
+
