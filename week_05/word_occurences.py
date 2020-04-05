0846e7bd675aff6b7d17836d12a8ed0e58bb4227
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2020-04-05 21:58:10 +1000

    add word count tally loop

--- week_05/word_occurences.py
+++ week_05/word_occurences.py
@@ -1,4 +1,8 @@
 string = input("Enter sequence of words: ")
 words = string.split(' ')
 
+amount_of_every_word = {}
+for word in words:
+    word_count = amount_of_every_word.get(word, 0)
+    amount_of_every_word[word] = word_count + 1
 
