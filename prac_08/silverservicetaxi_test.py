63c46f6a8bc5fd1e5f13a204fd0f6e0e22a71b60
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2020-05-03 22:48:16 +1000

    add rounding for get fare method

--- prac_08/silverservicetaxi_test.py
+++ prac_08/silverservicetaxi_test.py
@@ -3,6 +3,8 @@ from prac_08.silverservicetaxi import SilverServiceTaxi
 def main():
     ss_taxi = SilverServiceTaxi("Hummer", 100, 2)
     ss_taxi.drive(18)
+    fare = ss_taxi.get_fare()
+    print(fare)
     print(ss_taxi)
 
 main()
